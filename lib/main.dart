56761aa448873451ba9d12bf1449597644f9f22e
Kauê Murakami <kauetmurakami@gmail.com>
2020-04-16 19:47:37 -0300

    criando interface de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:uberflutterapp/telas/home.dart';
 
+
+final ThemeData temaPadrao = ThemeData(
+  primaryColor: Color(0xff37474f),
+  accentColor: Color(0xff546e7a)
+);
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
   home: Home(),
--- lib/telas/cadastro.dart
+++ lib/telas/cadastro.dart
@@ -1,53 +1,57 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'cadastro.dart';
-class Home extends StatefulWidget {
+class Cadastro extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _CadastroState createState() => _CadastroState();
 }
 
-class _HomeState extends State<Home> {
+class _CadastroState extends State<Cadastro> {
 
   TextEditingController _controllerEmail = TextEditingController();
   TextEditingController _controllerSenha = TextEditingController();
-
+  TextEditingController _controllerNome = TextEditingController();
+  bool _tipoUsuarioPassageiro = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text("Cadastro"),
+      ),
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("images/fundo.png"),
-            fit: BoxFit.cover
-          ),
-        ),
         padding: EdgeInsets.all(16),
         child: Center(
           child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(bottom: 32),
-                  child: Image.asset("images/logo.png",
-                  width: 200,
-                  height: 159,
+                TextField(
+                  controller: _controllerNome,
+                  autofocus: true,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(
+                      fontSize: 20
+                  ),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "nome",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius : BorderRadius.circular(6)
+                      )
                   ),
                 ),
                 TextField(
                   controller: _controllerEmail,
-                  autofocus: true,
                   keyboardType: TextInputType.emailAddress,
                   style: TextStyle(
-                    fontSize: 20
+                      fontSize: 20
                   ),
                   decoration: InputDecoration(
-                    contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                    hintText: "e-mail",
-                    filled: true,
-                    fillColor: Colors.white,
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "e-mail",
+                      filled: true,
+                      fillColor: Colors.white,
                       border: OutlineInputBorder(
                           borderRadius : BorderRadius.circular(6)
                       )
@@ -70,13 +74,30 @@ class _HomeState extends State<Home> {
                   ),
                 ),
                 Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Text("Passageiro"),
+                      Switch(
+                        value: _tipoUsuarioPassageiro,
+                        onChanged: (bool value){
+                          setState(() {
+                            _tipoUsuarioPassageiro = value;
+                          });
+                        },
+                      ),
+                      Text("Motorista"),
+                    ],
+                  ),
+                ),
+                Padding(
                   padding: EdgeInsets.only(top: 16, bottom: 10),
                   child: RaisedButton(
                     child: Text(
-                        "Entrar",
+                      "Cadastrar",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20
+                          color: Colors.white,
+                          fontSize: 20
                       ),
                     ),
                     color: Color(0xff1ebbd8),
@@ -86,32 +107,15 @@ class _HomeState extends State<Home> {
                     },
                   ),
                 ),
-                Center(
-                  child: GestureDetector(
-                    child: Text(
-                        "Não tem conta? Cadastre-se",
-                      style:TextStyle(
-                        color: Colors.white
-                      )
-                    ),
-                    onTap: (){
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                           builder: (context) => Cadastro()
-                          )
-                      );
-                    },
-                  ),
-                ),
+
                 Padding(
                   padding: EdgeInsets.only(top: 16),
                   child: Center(
                     child: Text(
-                        "Erro",
+                      "Erro",
                       style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 20
+                          color: Colors.red,
+                          fontSize: 20
                       ),
                     ),
                   ),
