4032c5b9f18a0076f60ef888626376eaa3995e55
Kauê Murakami <kauetmurakami@gmail.com>
2020-04-24 19:22:44 -0300

    recuperando localização atual do usuario

--- lib/telas/painelPassageiro.dart
+++ lib/telas/painelPassageiro.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
-
+import 'package:geolocator/geolocator.dart';
 import 'package:uberflutterapp/routes/routes.dart';
 
 class PainelPassageiro extends StatefulWidget {
@@ -11,10 +11,13 @@ class PainelPassageiro extends StatefulWidget {
 }
 
 class _PainelPassageiroState extends State<PainelPassageiro> {
+  CameraPosition _posicaoCamera = CameraPosition(
+  target: LatLng(-23.563999, -46.653256),);
 
   List<String> itensMenu = [
     "Configurações", "Deslogar"
   ];
+
   Completer<GoogleMapController> _controller = Completer();
 
   _deslogarUsuario() async {
@@ -43,6 +46,51 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
     _controller.complete( controller );
   }
 
+  _recuperarUltimalocalizacao() async{
+    Position position = await Geolocator()
+        .getLastKnownPosition(desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      if(position != null){
+        _posicaoCamera = CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 19
+        );
+        _movimentarCamera(_posicaoCamera);
+      }else{
+
+      }
+    });
+  }
+  _adicionaListenerALocalizacao(){
+      var geolocator = Geolocator();
+      var locationOptions = LocationOptions(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 10,
+      );
+      geolocator.getPositionStream(locationOptions).listen((Position position){
+        _posicaoCamera = CameraPosition(
+          target: LatLng(position.latitude, position.longitude),
+          zoom: 19
+        );
+        _movimentarCamera(_posicaoCamera);
+      });
+  }
+
+  _movimentarCamera(CameraPosition cameraPosition) async{
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(cameraPosition)
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _recuperarUltimalocalizacao();
+    _adicionaListenerALocalizacao();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,10 +117,7 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
       body: Container(
         child: GoogleMap(
           mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(
-              target: LatLng(-23.563999, -46.653256),
-              zoom: 16
-          ),
+          initialCameraPosition: _posicaoCamera,
           onMapCreated: _onMapCreated,
         ),
       ),
